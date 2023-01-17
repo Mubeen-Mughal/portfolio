bcb0bccdb44a69bd66731d950b26126d4fdef853
Mubeen <mubeenarshad64@gmail.com>
2023-01-17 22:09:05 +0500

    portfolio

--- src/component/intro/Intro.js
+++ src/component/intro/Intro.js
@@ -1,8 +1,33 @@
 import '../intro/Intro.scss';
+import profileimg from '../../img/profile.png';
+import Downarrow from '../../img/downarrow.png';
+import { init } from 'ityped'
+import { useEffect,useRef } from 'react';
 export default function Intro() {
+
+  const textRef=useRef();
+
+  useEffect(() => {
+    init(textRef.current, { 
+      showCursor: true,
+     strings: ['Developer',"Designer","Content Creator"], 
+    });
+  },[]);
   return (
     <div className='intro' id="intro">
-      test
+      <div className='left'>
+        <div className='imgcontainer'>
+        <img src={profileimg} alt="Profile Image"/>
+        </div>
+      </div>
+      <div className='right'>
+      <div className='wrapper'>
+        <h2>Hi There, I'm</h2>
+        <h1>Mubeen Arshad</h1>
+        <h3>Freelance <span ref={textRef}></span></h3>
+        <img src={Downarrow} alt="Down arrow" className='arrow-img'/>
+      </div>
+      </div>
     </div>
   ) 
 }
