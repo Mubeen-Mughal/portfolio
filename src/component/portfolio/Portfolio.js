c2cf38c730fb0a8fe72548d16ab777040951c0fd
Mubeen <mubeenarshad64@gmail.com>
2023-01-18 21:50:36 +0500

    portfolio

--- src/component/portfolio/Portfolio.js
+++ src/component/portfolio/Portfolio.js
@@ -1,8 +1,42 @@
 import '../portfolio/portfolio.scss';
+import portfolio from '../../img/portfolio.jpg';
 export default function Portfolio() {
   return (
     <div className='portfolio' id="portfolio">
-    portfolio   
+    <h1>Portfolio</h1>
+    <ul>
+    <li className='active'>Featured</li>
+    <li>Web App</li>
+    <li>Mobile App</li>
+    <li>Designing</li>
+    <li>Branding</li>
+    </ul>  
+    <div className='container'>
+    <div className='item'>
+      <img src={portfolio} alt="portfolio"/>
+      <h3>Branding</h3>
+    </div>
+    <div className='item'>
+      <img src={portfolio} alt="portfolio"/>
+      <h3>Branding</h3>
+    </div>
+    <div className='item'>
+      <img src={portfolio} alt="portfolio"/>
+      <h3>Branding</h3>
+    </div>
+    <div className='item'>
+      <img src={portfolio} alt="portfolio"/>
+      <h3>Branding</h3>
+    </div>
+    <div className='item'>
+      <img src={portfolio} alt="portfolio"/>
+      <h3>Branding</h3>
+    </div>  
+    <div className='item'>
+      <img src={portfolio} alt="portfolio"/>
+      <h3>Branding</h3>
+    </div>
+    </div> 
     </div>
   ) 
 }
